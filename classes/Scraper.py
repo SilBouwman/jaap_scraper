6f497e8a950a24672dc71bdc7ff3925dc28040ea
Sil Bouwman <silke.bouwman@incentro.com>
2019-11-17 17:48:23 +0100

    added a simple scraper function

--- classes/Scraper.py
+++ classes/Scraper.py
@@ -1,15 +1,17 @@
 # Importing external libraries
 from bs4 import BeautifulSoup
-import datetime
+import pandas as pd
 import requests
+import re
+import time
 
 # Importing internal libraries
-from util.utilFunctions import largestNumber
+from util.utilFunctions import largestNumber, hasNumbers, get_page
 
 
 
 class Scraper:
-    def __init__(self, soort, stad_url, huissoort=None):
+    def __init__(self, soort="koophuizen", stad_url="noord+brabant/west-noord-brabant/breda", huissoort=None):
         self.soort = soort if soort in ["koophuizen", "huurhuizen"] else None
         self.stad_url = stad_url
         self.huissoort = huissoort if huissoort in ["appartement", "woonhuis"] else None
@@ -19,37 +21,53 @@ class Scraper:
 
 
     def _get_n_pages(self):
-        headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0"}
-        response = requests.get(self.url, headers=headers)
-        soup = BeautifulSoup(response.text, "html.parser")
+        soup = get_page(self.url)
         n_pages_string = soup.find("span", {"class": "page-info"})
         n_pages = largestNumber(n_pages_string.text)
         return n_pages
 
     def _get_links(self):
-        headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0"}
         for current_page in range(0, self.n_pages):
             current_page += 1
-            response = requests.get(f"{self.url}/p{current_page}", headers=headers)
-            soup = BeautifulSoup(response.text, "html.parser")
+            soup = get_page(f"{self.url}/p{current_page}")
             for link in soup.findAll("a", {"class": "property-inner"}, href=True):
                 self.links.append(link["href"])
 
     def simple_scrape(self):
-        headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0"}
+        df = pd.DataFrame()
         for current_page in range(0, self.n_pages):
+            time.sleep(1)
             current_page += 1
-            response = requests.get(f"{self.url}/p{current_page}", headers=headers)
-            soup = BeautifulSoup(response.text, "html.parser")
+            print(f"page {current_page} of {self.n_pages}")
+            soup = get_page(f"{self.url}/p{current_page}")
             for house in soup.findAll("div", {"class": "property-info"}):
-                print(house.find("h2", {"class": "property-address-street"}).text)
-                print(house.find("div", {"class": "property-address-zipcity"}).text)
-                print(house.find("div", {"class": "property-features"}).text)
+                # Getting the address
+                address = house.find("h2", {"class": "property-address-street"}).text
 
+                # Getting the postal code
+                postal_code = house.find("div", {"class": "property-address-zipcity"}).text
+                postal_code = postal_code[:7]
 
+                # Getting the price
+                price = house.find("div", {"class": "price-info"}).text
+                price = int(re.findall("\d+", price.replace(".", ""))[0]) if hasNumbers(price) else None
 
+                # Getting sold
+                sold = bool(True) if house.find("span", {"style": "color:#d32013; font-size: 15px; font-weight: bold;"}) else bool(False)
 
+                # Getting the house_type, number of rooms and square_meters
+                feature_list = [feature.text for feature in house.findAll("div", {"class": "property-feature"})]
+                house_type = feature_list[0] if feature_list else None
+                number_of_rooms = int(re.findall("\d+", feature_list[1])[0]) if len(feature_list) > 1 else None
+                square_meters = int(re.findall("\d+", feature_list[2])[0]) if len(feature_list) > 2 else None
 
+                # Making the JSON
+                row_json = {"address": address, "postal_code": postal_code, "price": price,
+                            "sold": sold, "house_type": house_type,
+                            "number_of_rooms": number_of_rooms, "square_meters": square_meters}
 
+                # Appending it to the dataframe
+                df = df.append(row_json, ignore_index=True)
 
+        return df
 
--- util/utilFunctions.py
+++ util/utilFunctions.py
@@ -1,5 +1,6 @@
 # Importing external libraries
-
+import requests
+from bs4 import BeautifulSoup
 
 # Importing internal libraries
 
@@ -9,3 +10,13 @@
 def largestNumber(in_str):
     l=[int(x) for x in in_str.split() if x.isdigit()]
     return max(l) if l else None
+
+
+def hasNumbers(inputString):
+    return any(char.isdigit() for char in inputString)
+
+def get_page(url):
+    headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0"}
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, "html.parser")
+    return soup
